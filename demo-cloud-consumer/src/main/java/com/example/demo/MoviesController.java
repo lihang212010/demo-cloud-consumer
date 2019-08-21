f9a23b51d7bea0c6216c71e9d5bebacc77bf6f01
lihang212010 <root@kali>
2019-08-22 00:01:48 +0800

    https://github.com/lihang212010/demo-cloud-consumer.git

---
+++ demo-cloud-consumer/src/main/java/com/example/demo/MoviesController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.bouncycastle.jcajce.provider.asymmetric.dsa.DSASigner.detDSA;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+
+import ch.qos.logback.classic.Logger;
+
+
+
+@RestController
+public class MoviesController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping("/user/{id}")
+	public Users findById(@PathVariable Long id) {
+		return this.restTemplate.getForObject("http://demo-cloud-user/"+id, Users.class);
+	}	
+		
+	} 
+
